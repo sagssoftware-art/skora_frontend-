f37d50c86766c593bafefcd7bd505d84c0362eea
Gagana Sandares <gaganasadhares@gmail.com>
2026-02-10 22:14:25 +0530

    update

--- src/pages/ClassTeacher/ClassTeacher.jsx
+++ src/pages/ClassTeacher/ClassTeacher.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import './classTeacher.css'
+import '../ClassTeacher/ClassTeacher.css';
 
 const ClassTeacher = () => {
     const { classId } = useParams(); 
